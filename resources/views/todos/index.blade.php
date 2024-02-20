ee6169808a5f696f3f2b7d34a1b4e7d68e2fd2b2
Umair Ahmad Bakht <umairahmadbakht44@gmail.com>
2024-02-21 01:01:15 +0400

    Update index.blade.php
    Delete functionalte

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -71,9 +71,12 @@
                                                 href="{{ route('todos.show', $todo->id) }}">View</a>
                                             <a class="inner btn btn-sm btn-info"
                                                 href="{{ route('todos.edit', $todo->id) }}">Edit</a>
-                                            <form action="" class="inner">
+                                            <form action="{{ route('todos.destroy', $todo->id) }}" method="POST" class="inner">
+                                                @csrf
+                                                @method('DELETE')
                                                 <input type="hidden" name="todo_id" value="{{ $todo->id }}" />
-                                                <input type="submit" class="btn btn-sm btn-danger" value="delete" />
+                                                {{-- <input type="submit" class="btn btn-sm btn-danger" value="delete" /> --}}
+                                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this todo?')">Delete</button>
                                             </form>
                                         </td>
                                     </tr>
